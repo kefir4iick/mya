e901075a9446d08d571621bc9afa0158bc7d2863
kefir4iick <lizardm17@icloud.com>
2025-02-27 21:49:32 +0200

    add new file

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include "dioph.h"
+#include <iostream>
+
+void runTests() {
+    {
+        auto [gcd, coef] = gcd_function(48, 18);
+        if (gcd != 6) {
+            std::cout << "Test failed: right answer 6, got " << gcd << std::endl;
+        } else {
+            std::cout << "Test passed: gcd_function(48, 18)" << std::endl;
+        }
+        
+    }
+    {
+        auto [gcd, coef] = gcd_function(6, 5);
+        if (gcd != 1) {
+            std::cout << "Test failed: right answer 6, got " << gcd << std::endl;
+        } else {
+            std::cout << "Test passed: gcd_function(48, 18)" << std::endl;
+        }
+    }
+    {
+        auto [gcd, coef] = gcd_function(134, 78);
+        if (gcd != 2) {
+            std::cout << "Test failed: right answer 2, got " << gcd << std::endl;
+        } else {
+            std::cout << "Test passed: gcd_function(48, 18)" << std::endl;
+        }
+    }
+
+}
+
+int main() {
+    runTests();
+    return 0;
+}
