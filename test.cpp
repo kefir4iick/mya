d3b9851884602120c2c76ca0ee11ee7f37f034c7
kefir4iick <lizardm17@icloud.com>
2025-03-02 16:19:45 +0200

    add some tests

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ void runTests() {
     {
         auto [gcd, coef] = gcd_function(48, 18);
         if (gcd != 6) {
-            std::cout << "Test failed: right answer 6, got " << gcd << std::endl;
+            std::cout << "Test failed: gcd_function(48, 18) - expected 6, got " << gcd << std::endl;
         } else {
             std::cout << "Test passed: gcd_function(48, 18)" << std::endl;
         }
@@ -25,6 +25,22 @@ void runTests() {
             std::cout << "Test failed: right answer 2, got " << gcd << std::endl;
         } else {
             std::cout << "Test passed: gcd_function(48, 18)" << std::endl;
+        }
+    }
+    {
+        auto [gcd, coef] = gcd_function(0, 1);
+        if (gcd != 1) {
+            std::cout << "Test failed: right answer 1, got " << gcd << std::endl;
+        } else {
+            std::cout << "Test passed: gcd_function(0, 1)" << std::endl;
+        }
+    }
+    {
+        auto [gcd, coef] = gcd_function(1, 0);
+        if (gcd != 1) {
+            std::cout << "Test failed: right answer 1, got " << gcd << std::endl;
+        } else {
+            std::cout << "Test passed: gcd_function(0, 1)" << std::endl;
         }
     }
 
